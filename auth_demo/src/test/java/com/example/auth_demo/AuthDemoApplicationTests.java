d3b2da910b9a802d482214a66e24e2e0619fb07b
rgt <rasbingodarthapa@gmail.com>
2026-02-24 13:15:58 +0545

    auth demo project started

---
+++ auth_demo/src/test/java/com/example/auth_demo/AuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
