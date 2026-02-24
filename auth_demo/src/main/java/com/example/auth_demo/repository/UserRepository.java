5f5115b8c044e4ad80f508580e6096ba8eda11a3
rgt <rasbingodarthapa@gmail.com>
2026-02-24 19:35:13 +0545

    update minor things

--- auth_demo/src/main/java/com/example/auth_demo/repository/UserRepository.java
+++ auth_demo/src/main/java/com/example/auth_demo/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.auth_demo.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.auth_demo.entity.AppUser;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long>{
     Optional<AppUser> findByUsername(String username);
 }
