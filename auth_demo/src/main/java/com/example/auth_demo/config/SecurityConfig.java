78876cf927a775801e6a35148475094d20351fdb
rgt <rasbingodarthapa@gmail.com>
2026-02-24 18:58:05 +0545

    update custom security config

--- auth_demo/src/main/java/com/example/auth_demo/config/SecurityConfig.java
+++ auth_demo/src/main/java/com/example/auth_demo/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.auth_demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +21,11 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
                 .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated());
-        http.formLogin( form -> form.permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults()); // for postman(haven't understood much)
+
+        // New session id on each request (session based auth is not used now)
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
